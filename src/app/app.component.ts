bd5e7e7a8f6bdc4a907544dde80ee1ec4d3edc34
lidya <lidya.s164@gmail.com>
2021-09-22 10:57:33 +0300

    ++flash list,add and edit templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IFlash } from './models/flash.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,96 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-flashcards';
+  @ViewChild('flashForm', {static: true}) flashForm!: NgForm
+  flashs: IFlash[] = [{
+    question: 'Question 1',
+    answer: 'Answer 1',
+    show: false,
+    id: this.getRandomNumber(),
+    }, {
+    question: 'Question 2',
+    answer: 'Answer 2',
+    show: false,
+    id: this.getRandomNumber(),
+    remembered: 'correct'
+    }, {
+    question: 'Question 3',
+    answer: 'Answer 3',
+    show: false,
+    id: this.getRandomNumber(),
+    remembered: 'incorrect'
+
+    }];
+    flash: IFlash = {
+      question: '',
+      answer: '',
+    };
+    editing = false;
+    editingId!: number;
+    trackByFlashId(index: any, flash: any) {
+      return flash.id;
+    }
+
+  getRandomNumber() {
+    return Math.floor(Math.random() * 10000);
+   }
+   handleToggleCard(id: number){
+     const flash = this.flashs.find(flash => flash.id == id);
+     // @ts-ignore
+     flash?.show = !flash?.show
+   }
+   handleDelete(id: number) {
+     // @ts-ignore
+    const flashId = this.flashs.indexOf(flash => flash.id ===
+    id);
+    this.flashs.splice(flashId, 1)
+    }
+    handleEdit(id: number) {
+    this.editing = true;
+    this.editingId = id;
+    //@ts-ignore
+    const flash = this.flashs.find(flash => flash.id === id);
+    //@ts-ignore
+    this.flash.question = flash.question;
+    //@ts-ignore
+    this.flash.answer = flash.answer;
+    }
+     // @ts-ignore
+    handleRememeberedChange({id, flag}) {
+    const flash = this.flashs.find(flash => flash.id === id);
+     // @ts-ignore
+    flash.remembered = flag;
+    }
+
+    handleSubmit()
+ {
+  this.flashs.push({
+    id: this.getRandomNumber(),
+    ...this.flash
+  });
+  this.handleClear();
+ }
+    handleClear() {
+      this.flash = {
+        question: '',
+        answer: ''
+      }
+      this.flashForm.reset();
+    }
+    handleUpdate() {
+      const flash = this.flashs.find(flash => flash.id ===
+        this.editingId);
+        //@ts-ignore
+        flash.question = this.flash.question;
+        //@ts-ignore
+        flash.answer = this.flash.answer;
+        this.handleCancel();
+       
+    }
+    handleCancel() {
+      this.editing = false;
+      //@ts-ignore
+      this.editingId = undefined;
+      this.handleClear();
+      }
 }
